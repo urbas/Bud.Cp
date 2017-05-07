c5196b569857aec2f35cfd94af438d60e4403c05
Matej Urbas <matej.urbas@gmail.com>
2017-05-07 12:33:09 +0100

    Initial commit.

---
+++ Bud.Cp/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bud.Cp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matej Urbas")]
+[assembly: AssemblyProduct("Bud.Cp")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5FDD6E0D-C1B4-48E8-96D1-CBB11D0BC774")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,5 @@
+[![NuGet](https://img.shields.io/nuget/v/Bud.Cp.svg)](https://www.nuget.org/packages/Bud.Cp/)
+
+
+
+# Bud.Cp
